f64f5c704068d47adc12eecdd4d91341e15f433c
Przemyslaw Lusnia <774194+Swirastlynn@users.noreply.github.com>
2023-03-28 15:18:13 +0200

    immutable able to be json serialized data model

--- lib/weather/data/weather_response_dto.dart
+++ lib/weather/data/weather_response_dto.dart
@@ -1,4 +1,10 @@
-class WeatherResponseDTO {
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'weather_response_dto.g.dart';
+
+@JsonSerializable()
+class WeatherResponseDTO extends Equatable {
   final Coord coord;
   final List<Weather> weather;
   final String base;
@@ -12,7 +18,8 @@ class WeatherResponseDTO {
   final int id;
   final String name;
   final int cod;
-  WeatherResponseDTO({
+
+  const WeatherResponseDTO({
     required this.coord,
     required this.weather,
     required this.base,
@@ -27,74 +34,180 @@ class WeatherResponseDTO {
     required this.name,
     required this.cod,
   });
+
+  factory WeatherResponseDTO.fromJson(Map<String, dynamic> json) =>
+      _$WeatherResponseDTOFromJson(json);
+
+  Map<String, dynamic> toJson() => _$WeatherResponseDTOToJson(this);
+
+  @override
+  List<Object?> get props => [
+        coord,
+        weather,
+        base,
+        main,
+        visibility,
+        wind,
+        clouds,
+        dt,
+        sys,
+        timezone,
+        id,
+        name,
+        cod,
+      ];
 }
 
-class Coord {
+@JsonSerializable()
+class Coord extends Equatable {
   final double lon;
   final double lat;
-  Coord({
+
+  const Coord({
     required this.lon,
     required this.lat,
   });
+
+  factory Coord.fromJson(Map<String, dynamic> json) => _$CoordFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CoordToJson(this);
+
+  @override
+  List<Object?> get props => [
+        lon,
+        lat,
+      ];
 }
 
-class Weather {
+@JsonSerializable()
+class Weather extends Equatable {
   final int id;
   final String main;
   final String description;
   final String icon;
-  Weather({
+
+  const Weather({
     required this.id,
     required this.main,
     required this.description,
     required this.icon,
   });
+
+  factory Weather.fromJson(Map<String, dynamic> json) =>
+      _$WeatherFromJson(json);
+
+  Map<String, dynamic> toJson() => _$WeatherToJson(this);
+
+  @override
+  List<Object?> get props => [
+        id,
+        main,
+        description,
+        icon,
+      ];
 }
 
-class Main {
+@JsonSerializable()
+class Main extends Equatable {
   final double temp;
-  final double feels_like;
-  final double temp_min;
-  final double temp_max;
+  @JsonKey(name: 'feels_like')
+  final double feelsLike;
+  @JsonKey(name: 'temp_min')
+  final double tempMin;
+  @JsonKey(name: 'temp_max')
+  final double tempMax;
   final int pressure;
   final int humidity;
-  Main({
+
+  const Main({
     required this.temp,
-    required this.feels_like,
-    required this.temp_min,
-    required this.temp_max,
+    required this.feelsLike,
+    required this.tempMin,
+    required this.tempMax,
     required this.pressure,
     required this.humidity,
   });
+
+  factory Main.fromJson(Map<String, dynamic> json) => _$MainFromJson(json);
+
+  Map<String, dynamic> toJson() => _$MainToJson(this);
+
+  @override
+  List<Object?> get props => [
+        temp,
+        feelsLike,
+        tempMin,
+        tempMax,
+        pressure,
+        humidity,
+      ];
 }
 
-class Wind {
+@JsonSerializable()
+class Wind extends Equatable {
   final double speed;
   final int deg;
-  Wind({
+
+  const Wind({
     required this.speed,
     required this.deg,
   });
+
+  factory Wind.fromJson(Map<String, dynamic> json) => _$WindFromJson(json);
+
+  Map<String, dynamic> toJson() => _$WindToJson(this);
+
+  @override
+  List<Object?> get props => [
+        speed,
+        deg,
+      ];
 }
 
-class Clouds {
+@JsonSerializable()
+class Clouds extends Equatable {
   final int all;
-  Clouds({
+
+  const Clouds({
     required this.all,
   });
+
+  factory Clouds.fromJson(Map<String, dynamic> json) => _$CloudsFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CloudsToJson(this);
+
+  @override
+  List<Object?> get props => [
+        all,
+      ];
 }
 
-class Sys {
+@JsonSerializable()
+class Sys extends Equatable {
   final int type;
   final int id;
   final String country;
   final int sunrise;
   final int sunset;
-  Sys({
+
+  const Sys({
     required this.type,
     required this.id,
     required this.country,
     required this.sunrise,
     required this.sunset,
   });
+
+  factory Sys.fromJson(Map<String, dynamic> json) => _$SysFromJson(json);
+
+  Map<String, dynamic> toJson() => _$SysToJson(this);
+
+  @override
+  List<Object?> get props => [
+        type,
+        id,
+        country,
+        sunrise,
+        sunset,
+      ];
 }
