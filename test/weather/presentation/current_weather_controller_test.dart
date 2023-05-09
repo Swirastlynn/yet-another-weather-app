f7bae3bf1d4d35e66b1b4a513478a3803c33a8c7
Przemysław Luśnia <lusp@viessmann.com>
2023-05-09 13:50:14 +0200

    unit tests setup - failure case

--- test/weather/presentation/current_weather_controller_test.dart
+++ test/weather/presentation/current_weather_controller_test.dart
@@ -4,6 +4,7 @@ import 'package:mocktail/mocktail.dart';
 import 'package:yet_another_weather_app/common/tools/either_extensions.dart';
 import 'package:yet_another_weather_app/weather/application/weather_use_case.dart';
 import 'package:yet_another_weather_app/weather/domain/current_weather_model.dart';
+import 'package:yet_another_weather_app/weather/domain/weather_failure.dart';
 import 'package:yet_another_weather_app/weather/providers.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
@@ -91,6 +92,52 @@ void main() {
           listener(AsyncValue.data(emptyData), any(that: isA<AsyncLoading>())),
       () => listener(
           any(that: isA<AsyncLoading>()), const AsyncValue.data(loadedData)),
+    ]);
+    verifyNoMoreInteractions(listener);
+    verify(() => weatherUseCase.getWeather(
+          cityId: any(named: 'cityId'),
+          appId: any(named: 'appId'),
+          units: any(named: 'units'),
+        )).called(1);
+  });
+
+  test('controller failure test', () async {
+    final weatherUseCase = MockWeatherUseCase();
+    final appLocalizations = MockAppLocalizations();
+    when(() => weatherUseCase.getWeather(
+          cityId: any(named: 'cityId'),
+          appId: any(named: 'appId'),
+          units: any(named: 'units'),
+        )).thenAnswer(
+      (_) => failureFuture(WeatherFailure.unknown(
+          appLocalizations: appLocalizations, stackTrace: MockStackTrace())),
+    );
+    // TODO FakeAppLocalizations would be more appropriate - less code
+    when(() => appLocalizations.commonErrorTitle)
+        .thenAnswer((_) => "common error title");
+    when(() => appLocalizations.commonErrorMessage)
+        .thenAnswer((_) => "common error mesg");
+
+    final container = makeProviderContainer(weatherUseCase);
+    final listener = Listener<AsyncValue<CurrentWeatherModel>>();
+    // listen to the provider and call [listener] whenever its value changes
+    container.listen(
+      currentWeatherControllerProvider,
+      listener,
+      fireImmediately: true,
+    );
+    final controller =
+        container.read(currentWeatherControllerProvider.notifier);
+    // verify initial value from build method
+    verify(() => listener(null, AsyncData(emptyData)));
+    // run
+    await controller.getWeather();
+    // verify
+    verifyInOrder([
+      () =>
+          listener(AsyncValue.data(emptyData), any(that: isA<AsyncLoading>())),
+      () => listener(
+          any(that: isA<AsyncLoading>()), any(that: isA<AsyncError>())),
     ]);
     verifyNoMoreInteractions(listener);
     verify(() => weatherUseCase.getWeather(
