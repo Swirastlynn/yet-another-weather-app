a240e5640fe87957fb42eaa9faab4050ec398ef6
Damian Olszewski <OleD@viessmann.com>
2023-06-06 10:07:08 +0200

    add pl translate, add rules to analysis_options.yaml

--- lib/weather/providers.dart
+++ lib/weather/providers.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:yet_another_weather_app/weather/presentation/presentation_model/current_weather_presentation_model.dart';
 import '../common/providers.dart';
-import '../common/tools/app_localizations_utils.dart';
+import '../localization/localizations_provider.dart';
 import 'application/weather_use_case.dart';
 import 'data/weather_api_data_source.dart';
 import 'presentation/current_weather_controller.dart';
